12d1102fbd1bc3a77aae5b5a422ac282994316a3
Arshan Khan <khanarshan2000@outlook.com>
2018-11-22 14:21:47 +0530

    Push Hello World Files
    Hello World Files

---
+++ HelloWorldConsole/src/HelloWorldConsole.java
@@ -0,0 +1,10 @@
+
+public class HelloWorldConsole {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World Console App");
+		System.out.println("and this is second line");
+	}
+
+}
